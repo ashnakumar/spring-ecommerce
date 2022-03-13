7b384ef6d1327ee54135722193073119928a145a
Ashna Kumar <ashna.kumar1998@gmail.com>
2022-03-13 11:56:08 +0530

    routing

--- src/main/java/com/ashna/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/ashna/ecommerce/dao/ProductRepository.java
@@ -1,12 +1,16 @@
 package com.ashna.ecommerce.dao;
 
+import com.ashna.ecommerce.entity.Product;
 import com.ashna.ecommerce.entity.ProductCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductRepository extends JpaRepository<ProductCategory, Long> {
-
+        Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
